c08fb910c81fee4278129db2cc8448898b3779fa
waandaa <wandahopkinson@mmc.school.nz>
2025-08-19 09:17:17 +1200

    Display message version 1
    decoration "==="

---
+++ C_01_Display_Message.V1.py
@@ -0,0 +1,11 @@
+# Functions go here
+def make_statement(statement: object, decoration: object):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main Routine goes here
+print()
+make_statement("Kia Ora and Welcome to 2025’s New Zealand International Film Festival", "=")
